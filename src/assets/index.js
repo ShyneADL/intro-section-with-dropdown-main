484cbcd7f4ba6cd9f910710fd3feb577d2d8f6aa
Ifeanyi Aladi <benedictaladi@gmail.com>
2023-10-14 14:10:31 +0100

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import hero from './image-hero-desktop.png'
+import heroM from './image-hero-mobile.png'
+import close from './icon-close-menu.svg'
+import menu from './icon-menu.svg'
+
+export {
+    hero,
+    heroM,
+    close,
+    menu,
+}
